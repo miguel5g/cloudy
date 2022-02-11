5c71952c0eefb14a671164073cc654ceaf05e343
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-11 00:07:44 -0300

    chore: Decreased the padding

--- src/components/Apps.tsx
+++ src/components/Apps.tsx
@@ -4,7 +4,7 @@ import { AppLink } from './AppLink';
 
 export const Apps = () => {
   return (
-    <aside className="flex flex-col justify-center p-8 border-r border-gray-200">
+    <aside className="flex flex-col justify-center p-4 border-r border-gray-200">
       <nav>
         <AppLink href="/" aria-label="Conversas">
           <FiMessageSquare />
