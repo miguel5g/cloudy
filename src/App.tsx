12cedd105f4a18cc19809211441b4e75aaf879d0
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-10 22:53:32 -0300

    feat: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import { AppRoutes } from './Routes';
+import './styles/globals.css';
+
+export const App = () => {
+  return <AppRoutes />;
+};
---
+++ src/Routes.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+export const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </Router>
+  );
+};
