21cc6751407ac0ea59ca64deef64ede3acbd9cd8
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-11 01:28:58 -0300

    chore: Added overflow auto

--- src/components/ChatInfo/index.tsx
+++ src/components/ChatInfo/index.tsx
@@ -2,7 +2,7 @@ import { ChatMember } from './ChatMember';
 
 export const ChatInfo = () => {
   return (
-    <div className="flex flex-col w-full max-w-sm p-4 border-l border-gray-200">
+    <div className="flex flex-col w-full max-w-sm p-4 overflow-y-auto border-l border-gray-200">
       <div className="flex items-center justify-between px-4 py-2">
         <h2 className="font-light text-slate-900">Membros</h2>
       </div>
