70b120cee78b9119a66f01556eaf35c335e5092a
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-12 02:24:45 -0300

    chore: Updated application name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ export const Header = () => {
   return (
     <header className="flex px-8 py-4 border-b border-gray-200">
       <h1 className="flex items-center gap-2">
-        <FiMessageSquare className="text-2xl text-sky-500" strokeWidth={3} />
-        <span className="text-2xl font-bold">Brand</span>
+        <FiMessageSquare className="text-3xl text-sky-500" />
+        <span className="text-2xl font-bold">Cloudy</span>
       </h1>
 
       <div className="ml-auto" />
