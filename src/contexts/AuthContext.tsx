bac1ca28f78685d4520c7f9e69109170d54eb95d
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-12 02:16:27 -0300

    feat: Created AuthContext

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, useEffect, useState } from 'react';
+
+import { User } from '../typings';
+
+interface AuthContextType {
+  isLoading: boolean;
+  isAuthenticated: boolean;
+  user: User | null;
+}
+
+export const authContext = createContext({} as AuthContextType);
+
+export const AuthContextProvider: React.FC = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    /* TODO: Implement auth checker */
+    setTimeout(() => {
+      setIsLoading(false);
+      setUser({
+        id: '1',
+        name: 'John Doe',
+        avatarUrl: 'https://via.placeholder.com/64',
+      });
+    }, 200);
+  }, []);
+
+  return (
+    <authContext.Provider
+      value={{
+        isLoading,
+        isAuthenticated: !!user,
+        user,
+      }}
+    >
+      {children}
+    </authContext.Provider>
+  );
+};
