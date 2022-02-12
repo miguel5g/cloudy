69a909859305eb1ffe727c491aa5cdc3dbc9325e
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-12 00:48:49 -0300

    chore: Update y padding

--- src/components/Chats/ChatsHeader.tsx
+++ src/components/Chats/ChatsHeader.tsx
@@ -2,7 +2,7 @@ import { FiPlus } from 'react-icons/fi';
 
 export const ChatsHeader = () => {
   return (
-    <div className="flex items-center justify-between px-4 py-2">
+    <div className="flex items-center justify-between px-4 pt-2">
       <h2 className="font-light text-slate-900">Conversas</h2>
 
       <button className="flex items-center p-1 transition hover:opacity-75">
