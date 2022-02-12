69a1e14c11f1b6c88f11fe6da8ffc0076296382c
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-12 00:46:12 -0300

    feat: Created Instructions component

---
+++ src/components/Instructions.tsx
@@ -0,0 +1,16 @@
+import choseSVG from '../assets/svg/choose.svg';
+
+export const Instructions = () => {
+  return (
+    <div className="flex flex-col items-center justify-center flex-1 p-8 bg-slate-50">
+      <img className="max-w-xs mb-4" src={choseSVG} alt="Chose" />
+
+      <h2 className="text-xl font-semibold text-slate-900">
+        Abra um chat para começar a conversar.
+      </h2>
+      <p className="text-sm font-light">
+        Você pode criar um novo chat ou entrar em um chat existente.
+      </p>
+    </div>
+  );
+};
