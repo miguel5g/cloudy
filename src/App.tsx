177dfe7f1e5a11b67117f335e2971e7c256f43db
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-12 02:26:03 -0300

    chore: Implemented auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { AuthContextProvider } from './contexts/AuthContext';
 import { AppRoutes } from './Routes';
 
 import './styles/globals.css';
 
 export const App = () => {
-  return <AppRoutes />;
+  return (
+    <AuthContextProvider>
+      <AppRoutes />
+    </AuthContextProvider>
+  );
 };
