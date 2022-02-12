7ea094b518778c554b3dc368ce89d691bdba2443
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-12 00:49:33 -0300

    chore: Update text styles

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -5,10 +5,10 @@ export const Instructions = () => {
     <div className="flex flex-col items-center justify-center flex-1 p-8 bg-slate-50">
       <img className="max-w-xs mb-4" src={choseSVG} alt="Chose" />
 
-      <h2 className="text-xl font-semibold text-slate-900">
+      <h2 className="text-2xl font-semibold text-slate-900">
         Abra um chat para começar a conversar.
       </h2>
-      <p className="text-sm font-light">
+      <p className="text-sm font-light text-slate-600">
         Você pode criar um novo chat ou entrar em um chat existente.
       </p>
     </div>
