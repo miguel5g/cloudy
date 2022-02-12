299f218695b8964eec5bfcb8e6d9bc2b0c825160
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-12 02:05:19 -0300

    feat: Implemented dinamic chat featch

--- src/components/ChatInfo/index.tsx
+++ src/components/ChatInfo/index.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { getChat } from '../../lib/Chats';
+import { Chat } from '../../typings';
+
 import { ChatMember } from './ChatMember';
 
 export const ChatInfo = () => {
+  const [chat, setChat] = useState<Chat | null>(null);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const id = params.id;
+
+    if (!id) return;
+
+    getChat(id).then(setChat);
+  }, []);
+
   return (
     <div className="flex flex-col w-full max-w-sm p-4 overflow-y-auto border-l border-gray-200">
       <div className="flex items-center justify-between px-4 py-2">
@@ -8,15 +26,12 @@ export const ChatInfo = () => {
       </div>
 
       <ul className="flex flex-col gap-px">
-        <li>
-          <ChatMember />
-        </li>
-        <li>
-          <ChatMember />
-        </li>
-        <li>
-          <ChatMember />
-        </li>
+        {chat &&
+          chat.members.map((member) => (
+            <li key={member.id}>
+              <ChatMember member={member} />
+            </li>
+          ))}
       </ul>
     </div>
   );
