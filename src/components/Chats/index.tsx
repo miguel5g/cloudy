ce9433032523bb11cb89e97c5e8bfe80fc86a1fa
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-12 01:29:33 -0300

    feat: Use dinamic chats

--- src/components/Chats/index.tsx
+++ src/components/Chats/index.tsx
@@ -1,22 +1,24 @@
 import { FiSend } from 'react-icons/fi';
+
 import { ChatLink } from './ChatLink';
 import { ChatsHeader } from './ChatsHeader';
 
+import { useChats } from '../../hooks/useChats';
+
 export const Chats = () => {
+  const { chats } = useChats();
+
   return (
     <section className="flex flex-col w-full max-w-sm gap-4 p-4 overflow-y-auto border-r border-gray-200">
       <ChatsHeader />
 
       <ul className="flex flex-col gap-px">
-        <li>
-          <ChatLink />
-        </li>
-        <li>
-          <ChatLink />
-        </li>
-        <li>
-          <ChatLink />
-        </li>
+        {chats &&
+          chats.map((chat) => (
+            <li key={chat.id}>
+              <ChatLink chat={chat} />
+            </li>
+          ))}
       </ul>
 
       <div className="flex items-center gap-4 px-4 py-3 mt-auto rounded-lg cursor-pointer bg-slate-300/20">
