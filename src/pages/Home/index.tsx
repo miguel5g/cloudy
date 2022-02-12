dff16e391b3d9e7b035a0723b8e72ca73b09307c
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-12 00:50:53 -0300

    feat: Created home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,22 @@
+import { Apps } from '../../components/Apps';
+import { Chats } from '../../components/Chats';
+import { Header } from '../../components/Header';
+import { Instructions } from '../../components/Instructions';
+
 export const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex h-screen">
+      <Apps />
+
+      <section className="flex flex-col flex-1">
+        <Header />
+
+        <main className="flex flex-1 overflow-hidden">
+          <Chats />
+
+          <Instructions />
+        </main>
+      </section>
     </div>
   );
 };
