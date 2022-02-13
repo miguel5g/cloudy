baab50f302d7bdadb15bab3131e9acdf827db8b5
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-13 13:19:29 -0300

    feat: Implemented authenticated routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,19 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import { useAuth } from './hooks/useAuth';
+
 import { ChatOpen, Home, Instructions } from './pages/Home';
 import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
 
 export const AppRoutes = () => {
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<Instructions />} />
-          <Route path="chats/:id" element={<ChatOpen />} />
-        </Route>
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        {isAuthenticated ? (
+          <>
+            <Route path="/" element={<Home />}>
+              <Route index element={<Instructions />} />
+              <Route path="chats/:id" element={<ChatOpen />} />
+            </Route>
+          </>
+        ) : (
+          <>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </>
+        )}
       </Routes>
     </Router>
   );
