847cdecd1db0a83f4d150de1147a29b73500b39c
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-14 17:48:34 -0300

    feat: add react-hot-toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Toaster } from 'react-hot-toast';
+
 import { AuthContextProvider } from './contexts/AuthContext';
 import { ChatsContextProvider } from './contexts/ChatsContext';
 import { SocketContextProvider } from './contexts/SocketContext';
@@ -11,6 +13,7 @@ export const App = () => {
       <AuthContextProvider>
         <ChatsContextProvider>
           <AppRoutes />
+          <Toaster position="top-center" />
         </ChatsContextProvider>
       </AuthContextProvider>
     </SocketContextProvider>
