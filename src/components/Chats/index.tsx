b780c7cda59a45c8cd9d9a1c02831a96747dd596
Miguel Ângelo <miguelcg958@gmail.com>
2022-02-14 17:50:13 -0300

    chore: add visual feedback

--- src/components/Chats/index.tsx
+++ src/components/Chats/index.tsx
@@ -1,5 +1,6 @@
 import { FiAward } from 'react-icons/fi';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
 
 import { ChatLink } from './ChatLink';
 import { ChatsHeader } from './ChatsHeader';
@@ -12,6 +13,10 @@ export const Chats = () => {
   const { chats, isLoading } = useChats();
 
   if (isLoading) return <LoadingChats />;
+
+  function handleFeedbackOpen() {
+    toast.error('Ainda não foi implementado.');
+  }
 
   return (
     <section className="flex flex-col w-full max-w-sm gap-4 p-4 overflow-y-auto border-r border-gray-200">
@@ -26,10 +31,13 @@ export const Chats = () => {
           ))}
       </ul>
 
-      <div className="flex items-center gap-4 px-4 py-3 mt-auto transition rounded-lg cursor-pointer bg-gradient-to-br from-sky-500/10 to-sky-600/20 hover:opacity-75">
+      <button
+        className="flex items-center gap-4 px-4 py-3 mt-auto transition rounded-lg bg-gradient-to-br from-sky-500/10 to-sky-600/20 hover:opacity-75"
+        onClick={handleFeedbackOpen}
+      >
         <FiAward className="text-sky-500" />
         <span className="text-slate-900">Enviar feedbacks</span>
-      </div>
+      </button>
     </section>
   );
 };
